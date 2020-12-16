fefea78c55693c9d82e6072a8867be9679b5ad27
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:20:08 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,4 +1,6 @@
 def find_element_index(arr, val)
-  arr.each_with_index { |num, i| return i if num == val }
-  return nil
+  arr.each_with_index do |num, i|
+    return i if num == val
+  end
+  nil
 end
